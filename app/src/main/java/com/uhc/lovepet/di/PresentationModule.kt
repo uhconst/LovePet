f5b52958567fd91115ad355ecc59184eccb6cc01
Uryel <uryelhenrique.c@gmail.com>
2019-05-11 12:59:51 +0100

    Add login view model to DI

--- app/src/main/java/com/uhc/lovepet/di/PresentationModule.kt
+++ app/src/main/java/com/uhc/lovepet/di/PresentationModule.kt
@@ -1,13 +1,14 @@
 package com.uhc.lovepet.di
 
 import com.uhc.presentation.MainViewModel
+import com.uhc.presentation.login.LoginViewModel
 import org.koin.android.viewmodel.dsl.viewModel
-import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
 
     viewModel { MainViewModel() }
+    viewModel { LoginViewModel() }
 
 //    factory { TransactionRecyclerAdapter() }
 
