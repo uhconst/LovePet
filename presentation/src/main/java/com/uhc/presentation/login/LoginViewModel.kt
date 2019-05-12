10a0770f64b29bdc5d24f0415402b27a84051cf5
Uryel <uryelhenrique.c@gmail.com>
2019-05-12 17:17:05 +0100

    Create login click event

--- presentation/src/main/java/com/uhc/presentation/login/LoginViewModel.kt
+++ presentation/src/main/java/com/uhc/presentation/login/LoginViewModel.kt
@@ -1,10 +1,15 @@
 package com.uhc.presentation.login
 
+import com.uhc.domain.firebase.FirebaseRepo
 import com.uhc.presentation.ui.base.BaseViewModel
 
 /**
  * Created by Constancio on 2019-05-04.
  */
-class LoginViewModel : BaseViewModel() {
+class LoginViewModel(val firebaseRepo: FirebaseRepo) : BaseViewModel() {
 
+
+    fun onLoginClick() {
+        firebaseRepo.writeNewUser("teste2", "456") //todo
+    }
 }
