17968ab6e073098f93e457eb4f2557d5ef28fc06
Uryel <uryelhenrique.c@gmail.com>
2019-05-12 17:16:21 +0100

    Create DI for firebase

--- app/src/main/java/com/uhc/lovepet/di/DataModule.kt
+++ app/src/main/java/com/uhc/lovepet/di/DataModule.kt
@@ -1,5 +1,7 @@
 package com.uhc.lovepet.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.uhc.data.local.db.AppDatabase
 import com.uhc.data.remote.TransactionService
 import org.koin.android.ext.koin.androidApplication
@@ -12,4 +14,8 @@ val dataModule = module {
     single { get<AppDatabase>().transactionDao() }
 
     single { TransactionService.createTransactionService() }
+
+    single { FirebaseAuth.getInstance()}
+
+    single { FirebaseDatabase.getInstance()}
 }
--- app/src/main/java/com/uhc/lovepet/di/DomainModule.kt
+++ app/src/main/java/com/uhc/lovepet/di/DomainModule.kt
@@ -1,6 +1,8 @@
 package com.uhc.lovepet.di
 
+import com.uhc.data.firebase.FirebaseRepoImpl
 import com.uhc.data.repository.TransactionRepositoryImpl
+import com.uhc.domain.firebase.FirebaseRepo
 import com.uhc.domain.interactor.GetTransactionsUseCase
 import com.uhc.domain.repository.TransactionRepository
 import org.koin.dsl.module
@@ -10,4 +12,6 @@ val domainModule = module {
     single<TransactionRepository> { TransactionRepositoryImpl(get(), get()) }
 
     factory { GetTransactionsUseCase(get()) }
+
+    single { FirebaseRepoImpl(get()) as FirebaseRepo }
 }
