7b4a3805c17f8e9773a54b3a1e5a481f55153588
Uryel <uryelhenrique.c@gmail.com>
2019-05-18 19:12:32 +0100

    Implement login success and navigation to home screen

--- presentation/src/main/java/com/uhc/presentation/login/LoginViewModel.kt
+++ presentation/src/main/java/com/uhc/presentation/login/LoginViewModel.kt
@@ -2,19 +2,36 @@ package com.uhc.presentation.login
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.uhc.domain.exception.DefaultException
 import com.uhc.domain.firebase.auth.FirebaseAuthService
-import com.uhc.domain.model.User
+import com.uhc.presentation.livedata.EventLiveData
 import com.uhc.presentation.ui.base.BaseViewModel
+import com.uhc.presentation.utils.SingleLiveData
 
 /**
  * Created by Constancio on 2019-05-04.
  */
 class LoginViewModel(val firebaseAuth: FirebaseAuthService) : BaseViewModel() {
-    private val _user = MutableLiveData<User>()
-    val user: LiveData<User> get() = _user
+
+    val username = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
+    private val _events = EventLiveData<LoginEvents>()
+    val events: LiveData<LoginEvents> get() = _events
+
+    val error = SingleLiveData<DefaultException>()
 
     fun onLoginClick() {
-        firebaseAuth.login(user.value)
-//        firebaseRepo.writeNewUser("teste2", "456") //todo
+        subscribeCompletable(
+            observable = firebaseAuth.login(username.value.orEmpty(), password.value.orEmpty())
+                .doOnSubscribe { startProgress() }
+                .doFinally { stopProgress() },
+            complete = { _events.postValue(LoginEvents.GO_TO_HOME) },
+            error = { error.postValue(it) }
+        )
     }
+}
+
+enum class LoginEvents {
+    GO_TO_HOME
 }
