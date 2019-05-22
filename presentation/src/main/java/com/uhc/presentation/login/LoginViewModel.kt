1f6a78f03e8a9a612a6c8d07967dc170ca135d5d
Uryel <uryelhenrique.c@gmail.com>
2019-05-23 00:02:20 +0100

    Implement login error

--- presentation/src/main/java/com/uhc/presentation/login/LoginViewModel.kt
+++ presentation/src/main/java/com/uhc/presentation/login/LoginViewModel.kt
@@ -2,11 +2,9 @@ package com.uhc.presentation.login
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.uhc.domain.exception.DefaultException
 import com.uhc.domain.firebase.auth.FirebaseAuthService
 import com.uhc.presentation.livedata.EventLiveData
 import com.uhc.presentation.ui.base.BaseViewModel
-import com.uhc.presentation.utils.SingleLiveData
 
 /**
  * Created by Constancio on 2019-05-04.
@@ -19,19 +17,18 @@ class LoginViewModel(val firebaseAuth: FirebaseAuthService) : BaseViewModel() {
     private val _events = EventLiveData<LoginEvents>()
     val events: LiveData<LoginEvents> get() = _events
 
-    val error = SingleLiveData<DefaultException>()
-
     fun onLoginClick() {
-        subscribeCompletable(
+        subscribeSingle(
             observable = firebaseAuth.login(username.value.orEmpty(), password.value.orEmpty())
                 .doOnSubscribe { startProgress() }
                 .doFinally { stopProgress() },
-            complete = { _events.postValue(LoginEvents.GO_TO_HOME) },
-            error = { error.postValue(it) }
+            success = { _events.postValue(LoginEvents.GO_TO_HOME) },
+            error = { _events.postValue(LoginEvents.LOGIN_ERROR) }
         )
     }
 }
 
 enum class LoginEvents {
-    GO_TO_HOME
+    GO_TO_HOME,
+    LOGIN_ERROR
 }
