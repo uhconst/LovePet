e31eefcafd536396e18dbfb951c20d0dd06a04db
Uryel <uryelhenrique.c@gmail.com>
2019-05-23 00:01:11 +0100

    Change firebase auth to RX manipulation

--- data/src/main/java/com/uhc/data/firebase/auth/FirebaseAuthServiceImpl.kt
+++ data/src/main/java/com/uhc/data/firebase/auth/FirebaseAuthServiceImpl.kt
@@ -1,9 +1,12 @@
 package com.uhc.data.firebase.auth
 
 import android.util.Log
+import com.androidhuman.rxfirebase2.auth.rxSignInWithEmailAndPassword
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.uhc.domain.firebase.auth.FirebaseAuthService
-import io.reactivex.Completable
+import com.uhc.domain.model.LoginResponse
+import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 
 /**
@@ -13,23 +16,16 @@ class FirebaseAuthServiceImpl(
     private val auth: FirebaseAuth
 ) : FirebaseAuthService {
 
-    override fun login(username: String, password: String): Completable =
-        Completable.create { emitter ->
-            auth.signInWithEmailAndPassword(username, password)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        // todo replace with a login wrapper (to be created)
-                        Log.d("Login", "signInWithEmail:success")
-                        emitter.onComplete()
-                    } else {
-                        // todo replace with a login wrapper (to be created)
-                        Log.w(
-                            "Login",
-                            "signInWithEmail:failure",
-                            task.exception
-                        )
-                        emitter.onError(Throwable("Error signInWithEmail:"))
-                    }
-                }
-        }.subscribeOn(Schedulers.io())
+    override fun login(username: String, password: String): Single<LoginResponse> =
+        auth.rxSignInWithEmailAndPassword(username, password)
+            .map {
+                it.toLoginResponse()
+            }
+            .doOnError {
+                Log.e("Login error", it.message) // todo log
+            }.subscribeOn(Schedulers.io())
+
+    private fun AuthResult.toLoginResponse(): LoginResponse =
+        LoginResponse(this.user.email.orEmpty())
+
 }
--- domain/src/main/java/com/uhc/domain/firebase/auth/FirebaseAuthService.kt
+++ domain/src/main/java/com/uhc/domain/firebase/auth/FirebaseAuthService.kt
@@ -1,10 +1,11 @@
 package com.uhc.domain.firebase.auth
 
-import io.reactivex.Completable
+import com.uhc.domain.model.LoginResponse
+import io.reactivex.Single
 
 /**
  * Created by Constancio on 2019-05-14.
  */
 interface FirebaseAuthService {
-    fun login(username: String, password: String): Completable
+    fun login(username: String, password: String): Single<LoginResponse>
 }
