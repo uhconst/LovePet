ae2e91746d82fb69f11bad8074ba55a6a853de23
Uryel <uryelhenrique.c@gmail.com>
2019-05-25 15:35:24 +0100

    Add di for client register

--- app/src/main/java/com/uhc/lovepet/di/PresentationModule.kt
+++ app/src/main/java/com/uhc/lovepet/di/PresentationModule.kt
@@ -1,6 +1,7 @@
 package com.uhc.lovepet.di
 
 import com.uhc.presentation.MainViewModel
+import com.uhc.presentation.client.ClientRegisterViewModel
 import com.uhc.presentation.home.HomeViewModel
 import com.uhc.presentation.login.LoginViewModel
 import org.koin.android.viewmodel.dsl.viewModel
@@ -8,8 +9,16 @@ import org.koin.dsl.module
 
 val presentationModule = module {
 
+    /** Main */
     viewModel { MainViewModel() }
+
+    /** Login */
     viewModel { LoginViewModel(get()) }
+
+    /** Home */
     viewModel { HomeViewModel() }
+
+    /** Client Register */
+    viewModel { ClientRegisterViewModel() }
 
 }
