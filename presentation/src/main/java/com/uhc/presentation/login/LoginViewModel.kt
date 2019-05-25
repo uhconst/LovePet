e1c7662c754a984c042d2050ed1c9096f90df540
Uryel <uryelhenrique.c@gmail.com>
2019-05-25 17:52:30 +0100

    Add mock data in case build is debug

--- presentation/src/main/java/com/uhc/presentation/login/LoginViewModel.kt
+++ presentation/src/main/java/com/uhc/presentation/login/LoginViewModel.kt
@@ -3,6 +3,7 @@ package com.uhc.presentation.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.uhc.domain.firebase.auth.FirebaseAuthService
+import com.uhc.presentation.BuildConfig
 import com.uhc.presentation.ui.base.BaseViewModel
 import com.uhc.presentation.utils.EventLiveData
 
@@ -16,6 +17,13 @@ class LoginViewModel(val firebaseAuth: FirebaseAuthService) : BaseViewModel() {
 
     private val _events = EventLiveData<LoginEvents>()
     val events: LiveData<LoginEvents> get() = _events
+
+    init {
+        if (BuildConfig.DEBUG) {
+            username.value = "teste@teste.com"
+            password.value = "teste123"
+        }
+    }
 
     fun onLoginClick() {
         subscribeSingle(
