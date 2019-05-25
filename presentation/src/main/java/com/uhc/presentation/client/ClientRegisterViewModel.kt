47a857c5bcd93fd74dede2e649c818c1b30dba75
Uryel <uryelhenrique.c@gmail.com>
2019-05-25 15:33:13 +0100

    Implement client register screen

---
+++ presentation/src/main/java/com/uhc/presentation/client/ClientRegisterViewModel.kt
@@ -0,0 +1,10 @@
+package com.uhc.presentation.client
+
+import com.uhc.presentation.ui.base.BaseViewModel
+
+/**
+ * Created by Constancio on 2019-05-25.
+ */
+class ClientRegisterViewModel : BaseViewModel() {
+
+}
