5900ba536fd944d0c0b0187a5522c7bfc25fd0dd
Uryel <uryelhenrique.c@gmail.com>
2019-06-02 13:04:39 +0100

    Add save button and text binding

--- presentation/src/main/java/com/uhc/presentation/client/ClientRegisterViewModel.kt
+++ presentation/src/main/java/com/uhc/presentation/client/ClientRegisterViewModel.kt
@@ -1,12 +1,41 @@
 package com.uhc.presentation.client
 
 import androidx.lifecycle.MutableLiveData
+import com.uhc.domain.firebase.database.FirebaseDatabaseService
+import com.uhc.domain.model.Client
 import com.uhc.presentation.ui.base.BaseViewModel
 
 /**
  * Created by Constancio on 2019-05-25.
  */
-class ClientRegisterViewModel : BaseViewModel() {
-    val name = MutableLiveData<String>()
+class ClientRegisterViewModel(
+    val firebaseDatabaseService: FirebaseDatabaseService
+) : BaseViewModel() {
 
+    val name = MutableLiveData<String>()
+    val cpf = MutableLiveData<String>()
+    val email = MutableLiveData<String>()
+    val telefone1 = MutableLiveData<String>()
+    val telefone2 = MutableLiveData<String>()
+    val rua = MutableLiveData<String>()
+    val numero = MutableLiveData<String>()
+    val bairro = MutableLiveData<String>()
+    val cep = MutableLiveData<String>()
+
+    fun onSaveClick() {
+        firebaseDatabaseService.writeNewClient(getClient())
+
+    }
+
+    private fun getClient(): Client = Client(
+        name = name.value.orEmpty(),
+        cpf = cpf.value.orEmpty(),
+        email = email.value.orEmpty(),
+        telefone1 = telefone1.value.orEmpty(),
+        telefone2 = telefone2.value.orEmpty(),
+        rua = rua.value.orEmpty(),
+        numero = numero.value.orEmpty(),
+        bairro = bairro.value.orEmpty(),
+        cep = cep.value.orEmpty()
+    )
 }
