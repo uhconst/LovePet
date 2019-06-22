779eb8b14ab5b9721ade2ae800cd58e4b07dd432
Uryel <uryelhenrique.c@gmail.com>
2019-06-22 17:07:05 +0100

    Create client list view model

---
+++ presentation/src/main/java/com/uhc/presentation/client/ClientListViewModel.kt
@@ -0,0 +1,13 @@
+package com.uhc.presentation.client
+
+import com.uhc.domain.firebase.database.FirebaseDatabaseService
+import com.uhc.presentation.ui.base.BaseViewModel
+
+/**
+ * Created by Constancio on 2019-06-22.
+ */
+class ClientListViewModel(
+    private val firebaseDatabaseService: FirebaseDatabaseService
+) : BaseViewModel() {
+
+}
