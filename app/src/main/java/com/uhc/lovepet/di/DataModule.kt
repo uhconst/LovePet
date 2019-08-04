24cbc73a8f74f228c3c8c09044f08279f796c5a5
Uryel <uryelhenrique.c@gmail.com>
2019-08-04 14:51:38 +0100

    Remove unused service

--- app/src/main/java/com/uhc/lovepet/di/DataModule.kt
+++ app/src/main/java/com/uhc/lovepet/di/DataModule.kt
@@ -3,7 +3,6 @@ package com.uhc.lovepet.di
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.uhc.data.local.db.AppDatabase
-import com.uhc.data.remote.TransactionService
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
@@ -12,8 +11,6 @@ val dataModule = module {
     single { AppDatabase.createDatabase(androidApplication()) }
 
     single { get<AppDatabase>().clientDao() }
-
-    single { TransactionService.createTransactionService() }
 
     single { FirebaseAuth.getInstance()}
 
